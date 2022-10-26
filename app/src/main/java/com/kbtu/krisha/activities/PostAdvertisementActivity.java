a66d9ea7fc7f1fae7cae2eda9b1577e1f01b178d
Vitaliy Oleinik <oleynik624@gmail.com>
2022-10-26 21:19:05 +0600

    back: add more new

--- app/src/main/java/com/kbtu/krisha/activities/PostAdvertisementActivity.java
+++ app/src/main/java/com/kbtu/krisha/activities/PostAdvertisementActivity.java
@@ -3,8 +3,12 @@ package com.kbtu.krisha.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 
 import com.kbtu.krisha.R;
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
 public class PostAdvertisementActivity extends AppCompatActivity {
 
@@ -12,5 +16,28 @@ public class PostAdvertisementActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_advertisement);
+
+        //Start ProgressBar first (Set visibility VISIBLE)
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(() -> {
+            //Starting Write and Read data with URL
+            //Creating array for parameters
+            String[] field = new String[2];
+            field[0] = "apartment";
+            field[1] = "id";
+            //Creating array for data
+            String[] data = new String[2];
+            data[0] = "apartment";
+            data[1] = "id";
+            PutData putData = new PutData("https://localhost/save", "POST", field, data);
+            if (putData.startPut()) {
+                if (putData.onComplete()) {
+                    String result = putData.getResult();
+                    //End ProgressBar (Set visibility to GONE)
+                    Log.i("PutData", result);
+                }
+            }
+            //End Write and Read data with URL
+        });
     }
 }
