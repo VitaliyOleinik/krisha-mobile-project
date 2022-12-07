07f0cdcb21bffdfa350a206d808fe3d8180da4f1
Alisher <alisher110301@gmail.com>
2022-12-07 18:19:48 +0600

    front: +7th page, search result

---
+++ app/src/main/java/com/kbtu/krisha/activities/SearchResult.java
@@ -0,0 +1,16 @@
+package com.kbtu.krisha.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kbtu.krisha.R;
+
+public class SearchResult extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_result);
+    }
+}
